6af1d1a13d37a4b72ce7d3614b246d5c5b9a8a30
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2016-10-08 18:52:05 +0200

    fixed scroll status,sending indicator and send date

--- src/js/MessageList.js
+++ src/js/MessageList.js
@@ -20,23 +20,59 @@ let MessageList = {
 
         this._scope.__watch__('view.messageListElement.childElementCount', (value) => {
             console.log('message list length:', value);
-            
+
             if (value > 0) {
                 this._fixMessageListScroll();
             }
         });
+
+        window.addEventListener('resize', () => this._fixMessageListScroll(true));
     },
 
 
-    _fixMessageListScroll() {
+    _fixMessageListScroll(force) {
         let messageList = this.messageListElement.parentElement;
         let lastMessage = this.messageListElement.lastElementChild;
         let messages = this.currentMessages;
-        let force = (messages[messages.length - 1].inOrOut === 'out');
 
-        if (force || messageList.scrollTop > (messageList.scrollHeight - messageList.clientHeight - 200)) {
-            lastMessage.scrollIntoView();
+        force = force || (messages[messages.length - 1].inOrOut === 'out');
+
+        if (force || messageList.scrollTop > (messageList.scrollHeight - messageList.clientHeight - 500)) {
+            lastMessage && lastMessage.scrollIntoView();
         }
+    },
+
+    isSending(message) {
+        return message.status === 'sending';
+    },
+
+    formatDate(date) {
+        if (typeof date === 'string') {
+            date = new Date(date);
+        }
+
+        let rightNow = new Date();
+        let formatConfig = {
+            localeMatcher: 'lookup',
+            hour12: false,
+            hour: '2-digit',
+            minute: '2-digit',
+        };
+
+        if (rightNow.getFullYear() > date.getFullYear()) {
+            formatConfig.year = 'numeric';
+        }
+
+        if (rightNow.getMonth() > date.getMonth() || rightNow.getDate() > date.getDate()) {
+            formatConfig.month = '2-digit';
+            formatConfig.day = '2-digit';
+        }
+
+        if (date.toDateString() === 'Invalid Date') {
+            return '';
+        }
+
+        return Intl.DateTimeFormat([], formatConfig).format(date);
     },
 
     __proto__: ViewController,
